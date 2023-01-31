e0754473fa5ff8aaf930d84a65824e3ee28375ab
Jessicaibanez <124103664+Jessicaibanez@users.noreply.github.com>
2023-01-31 20:24:13 -0300

    Initial commit

---
+++ funcion.js
@@ -0,0 +1,5 @@
+
+document.getElementById("boton").onclick= function (){
+    console.log("Informacion complementaria")
+ document.getElementById ("demo").innerHTML ="Soy una persona proactiva, organizada y responsable, con buenas relaciones interpersonales. Siempre tengo la mejor disposicion para la realizacion de mis labores."
+}
